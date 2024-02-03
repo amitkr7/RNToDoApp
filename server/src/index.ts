078a9380a70fb56ec9de05e5dcf440a316436ef2
amitkr7 <amit840901@gmail.com>
2024-02-03 22:18:18 +0530

    Mongoose setup and db creation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import './db';
+import Note from './models/note';
 
 const app = express();
 
@@ -8,6 +9,17 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('<h1>Initial Setup<h1>');
+});
+
+app.post('/create', async (req, res) => {
+  const newNote = new Note({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  await newNote.save();
+
+  res.json({ message: 'Created Notes' });
 });
 
 app.listen(8000, () => {
