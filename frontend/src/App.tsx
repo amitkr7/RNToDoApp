8f87c193508c6e0edcf93fa15debb0d5e1bdc3c6
amitkr7 <amit840901@gmail.com>
2024-02-09 21:23:12 +0530

    Submitting Form and storing data on backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,54 @@
+import axios from 'axios';
+import { useState, ChangeEventHandler, FormEventHandler } from 'react';
 import NoteItem from './components/NoteItem';
 
 const App = () => {
+  const [values, setValues] = useState({ title: '', description: '' });
+
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await axios.post('http://localhost:8000/note/create', {
+      title: values.title,
+      description: values.description,
+    });
+  };
   return (
-    <div className='max-w-3xl mx-auto bg-white'>
-      <div className='shadow-md rounded p-5 space-y-4'>
+    <div className='max-w-3xl mx-auto space-y-6'>
+      <form
+        onSubmit={handleSubmit}
+        className='shadow-md rounded p-5 space-y-6 bg-white'>
         <h1 className='font-semibold text-2xl text-center'>Note Application</h1>
         <div>
           <input
             type='text'
             className='w-full border-b-2 border-gray-700 outline-none '
             placeholder='Title'
+            name='title'
+            value={values.title}
+            onChange={handleChange}
           />
         </div>
         <div>
           <textarea
             name='description'
             className='w-full border-b-2 border-gray-700 outline-none resize-none h-36'
-            placeholder='Description'></textarea>
+            placeholder='Description'
+            value={values.description}
+            onChange={handleChange}></textarea>
         </div>
         <div className='text-right'>
           <button className='bg-blue-500 text-white px-5 py-2 rounded'>
             Submit
           </button>
         </div>
-      </div>
+      </form>
 
       <NoteItem title='Note Title' />
     </div>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import noteRouter from './routers/note';
 
 import './db';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
